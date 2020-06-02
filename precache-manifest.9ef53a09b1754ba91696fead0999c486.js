7c3b5597fa410b640666d241b108b9aa9866fbf2
Andrey Azov <andrey@ebi.ac.uk>
2020-06-02 01:10:18 +0100

    Updates

---
+++ precache-manifest.9ef53a09b1754ba91696fead0999c486.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb025abd5528331fa1944959d07bbdf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e741fd85e2cd958f2a",
+    "url": "/static/css/main.6e34bb4e.chunk.css"
+  },
+  {
+    "revision": "50b996db3c06b478285e",
+    "url": "/static/js/2.d10ee1dd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d10ee1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e741fd85e2cd958f2a",
+    "url": "/static/js/main.f566ea97.chunk.js"
+  },
+  {
+    "revision": "fb42f0383e0f163b5d89",
+    "url": "/static/js/runtime-main.9648c6b9.js"
+  },
+  {
+    "revision": "0352b1b6b8a372a0f93d247b326fd9e2",
+    "url": "/static/media/browser.0352b1b6.svg"
+  },
+  {
+    "revision": "3856a86fdda19edc2b89ec11f0329c18",
+    "url": "/static/media/custom-download.3856a86f.svg"
+  },
+  {
+    "revision": "cb46d072bf52774a7b05b564a86a00ff",
+    "url": "/static/media/entity-viewer.cb46d072.svg"
+  },
+  {
+    "revision": "d5ec820bb15834457a9e5cfaab0d1372",
+    "url": "/static/media/help.d5ec820b.svg"
+  },
+  {
+    "revision": "4afd58f1f9148d7653b6804860a08787",
+    "url": "/static/media/home.4afd58f1.svg"
+  },
+  {
+    "revision": "897fcf8cc97eb083990963f155de2420",
+    "url": "/static/media/search.897fcf8c.svg"
+  },
+  {
+    "revision": "cdd019929d4a72b4dfb6f3d532c8c081",
+    "url": "/static/media/species-selector.cdd01992.svg"
+  }
+]);
